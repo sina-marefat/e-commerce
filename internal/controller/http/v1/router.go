0ff560315d23aecb929cc1f0708af67221ce1779
sina <sina.marefat@snapp.cab>
2023-12-03 18:15:23 +0330

    Test

--- internal/controller/http/v1/router.go
+++ internal/controller/http/v1/router.go
@@ -3,5 +3,7 @@ package v1
 import "github.com/gofiber/fiber/v2"
 
 func RegisterRoutes(server *fiber.App) {
-
+	server.Get("/test", func(c *fiber.Ctx) error {
+		return c.SendString("Hello")
+	})
 }

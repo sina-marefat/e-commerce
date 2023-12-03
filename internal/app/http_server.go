46a1e45be8368493c12071e695730ae7138b4f4e
sina <sina.marefat@snapp.cab>
2023-12-03 17:52:00 +0330

    Feat : add server to config

--- internal/app/http_server.go
+++ internal/app/http_server.go
@@ -1,18 +1,23 @@
 package app
 
 import (
+	"e-commerce/config"
 	v1 "e-commerce/internal/controller/http/v1"
 	"e-commerce/pkg/fiber"
+	"fmt"
 	"log"
 )
 
 func NewHttpServer() {
 
+	address := fmt.Sprintf("%s:%d", config.GetString("service.http.host"), config.GetInt("server.http.port"))
+
 	server := fiber.NewServer()
 
 	v1.RegisterRoutes(server)
 
-	err := server.Listen(":3000")
+	err := server.Listen(address)
+
 	if err != nil {
 		log.Fatalf("Failed to start server : %s", err)
 	}

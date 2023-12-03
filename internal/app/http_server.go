0a52a210b851741d99e3c4a429efd681e34e4234
sina <sina.marefat@snapp.cab>
2023-12-03 17:33:24 +0330

    Feat : added serve command

--- internal/app/http_server.go
+++ internal/app/http_server.go
@@ -1,5 +1,19 @@
 package app
 
+import (
+	v1 "e-commerce/internal/controller/http/v1"
+	"e-commerce/pkg/fiber"
+	"log"
+)
+
 func NewHttpServer() {
 
+	server := fiber.NewServer()
+
+	v1.RegisterRoutes(server)
+
+	err := server.Listen(":3000")
+	if err != nil {
+		log.Fatalf("Failed to start server : %s", err)
+	}
 }
